748a352f1624fbc5969a2e02f0f026d982ea395f
Aleksei Kamynin <zendzirou@gmail.com>
2010-05-17 22:24:20 +0400

    Added support for overwriting templates

--- bu.php
+++ bu.php
@@ -2,11 +2,15 @@
 class Bu{
     public static function view($_templateName, $_templateData=array()){
         extract($_templateData);
-        $_fileName = 'view/'.$_templateName.'.php';
+        $_fileName = 'view/core/'.$_templateName.'.php';
+        $_hostFileName = 'view/'.HTTP_HOST.'/'.$_templateName.'.php';
         ob_start();
-            if(!file_exists($_fileName))
+            if(file_exists($_hostFileName))
+                include($_hostFileName);
+            elseif(file_exists($_fileName))
+                include($_fileName);
+            else
                 throw new Exception ('Template '.$_templateName.' not exists!');
-            include($_fileName);
             $_content = ob_get_contents();
         ob_end_clean();
         return $_content;
